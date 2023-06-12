6728d51382ba8147cc14f8341b7c66e22c8b35aa
tebusss <villeterasahde@gmail.com>
2023-06-12 21:27:57 +0300

    refactoring gamegrid

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,6 +1,6 @@
 import { Text, SimpleGrid } from "@chakra-ui/react";
 import GameCard from "./GameCard";
-import useGames, { Platform } from "../hooks/useGames";
+import useGames from "../hooks/useGames";
 import GameCardSkeleton from "./GameCardSkeleton";
 import { GameQuery } from "../App";
 interface Props {
@@ -9,9 +9,9 @@ interface Props {
 const GameGrid = ({ gameQuery }: Props) => {
   const { data, error, isLoading } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+  if (error) return <Text>{error}</Text>;
   return (
     <>
-      {error && <Text>{error}</Text>}
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
         padding="10px"
